cd614353ab118577a29e4dd85c6a504d74c8101e
noname007 <soul11201@126.com>
2014-10-10 00:38:17 +0800

    qsort merge_sort 计算逆序数,计算的逆序数还有点问题

--- oj1-3.cpp
+++ oj1-3.cpp
@@ -4,20 +4,56 @@
 using namespace std;
 typedef struct
 {
-    int x[10^6],y[10^6];
+    int x,y;
 }Point;
 
 //int (*pf)(int *x,int y,int index1,int  index2);
 
-int cmp(int *x,int *y,int index1,int  index2)
+int cmp(const void * a,const void * b)
 {
-
+    Point *aa = (Point *)a;
+    Point *bb = (Point *)b;
+    if(aa->x == bb->x)
+    {
+        return bb->y - aa->y;
+    }
+    else
+    {
+        return  bb->x - aa->x;
+    }
 
 }
 
-int merge_sort(Point*,int,int,int,int (*pf)(int *x,int *y,int index1,int index2));
+int merge_sort(Point *arr,int start,int mid,int last)
+{
+    int *temp = new int[mid - start];
+    for(int i = start;i<mid;++i)
+    {
+        temp[i] = arr[i].y;
+    }
+    int k = start;
+    int reverse_order = 0;
 
-int _merge(Point * arr, int start, int last,int (*pf)(int *x,int *y,int index1,int index2))
+    for(int i = start, j = mid ; i < mid ; )
+    {
+        if(j < last &&  arr[j].y <= temp[i])
+        {
+            arr[k++].y = arr[j].y;
+            reverse_order += j-i;
+            ++j;
+            printf("i:%d k:%d j:%d\n",i,reverse_order,j);
+        }
+
+        if(last <= j || temp[i]<arr[j].y)
+        {
+            arr[k++].y= temp[i++];
+        }
+    }
+    delete []temp;
+    return reverse_order;
+}
+
+int _merge(Point * arr, int start, int last)
 {
     if(start +1 == last)
     {
@@ -25,48 +61,25 @@ int _merge(Point * arr, int start, int last,int (*pf)(int *x,int *y,int index1,i
     }
     int reverse_order=0;
     int mid = (start + last)>>1;
-    _merge(arr,start,mid,pf);
-    _merge(arr,mid,last,pf);
-    return reverse_order + merge_sort(arr,start,mid,last,pf);
+    _merge(arr,start,mid);
+    _merge(arr,mid,last);
+    return reverse_order + merge_sort(arr,start,mid,last);
 
 }
 
-int merge_sort(Point *arr,int start,int mid,int last,int (*pf)(int *x,int *y,int index1,int index2))
-{
-    int *temp = new int[mid - start];
-    for(int i = start;i<mid;++i)
-    {
-        temp[i] = arr->x[i];
-    }
-    int k = start;
-    int reverse_order = 0;
-
-    for(int i = start,j= mid;i<mid||j<last;++i,++j)
-    {
-//        if(temp[i] >= arr[j])
-//        {
-//            arr[k++] = arr[j];
-//            reverse_order += j-i;
-//        }
-//        else
-//        {
-//            arr[k++] = temp[i];
-//        }
-    }
-    delete []temp;
-    return reverse_order;
-}
 
 int main()
 {
     int lamb_tower;
-    Point P;
+    Point P[100005];
     scanf("%d",&lamb_tower);
 
 //    while(lamb_tower --)
-    for(int i =0;i,lamb_tower;++i)
+    for(int i =0;i<lamb_tower;++i)
     {
-        scanf("%d%d",&P.x[i],&P.y[i]);
+        scanf("%d%d",&(P[i].x),&(P[i].y));
     }
-    _merge(&P,0,lamb_tower,cmp);
+    qsort(P,lamb_tower, sizeof(P[0]),cmp);
+    int reverse_order = _merge(P,0,lamb_tower);
+    printf("%d\n",reverse_order);
 }
