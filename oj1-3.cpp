56d9136665aec65b0e6f69ee1fddaabe0ee58e2c
noname007 <soul11201@126.com>
2014-10-16 23:13:08 +0800

    我是大sb,居然函数手残的写成 返回int型o(╯□╰)o

--- oj1-3.cpp
+++ oj1-3.cpp
@@ -1,6 +1,6 @@
-#include<cstdio>
-#include<cstdlib>
-#include<iostream>
+#include<stdio.h>
+#include<stdlib.h>
+//#include<iostream>
 using namespace std;
 typedef struct
 {
@@ -12,28 +12,19 @@ int cmp(const void * a,const void * b)
 {
     Point *aa = (Point *)a;
     Point *bb = (Point *)b;
-    //x,y¾ù»¥Òì
-//    if(aa->x == bb->x)
-//    {
-//        return bb->y - aa->y;
-//    }
-//    else
-//    {
-        return  bb->x - aa->x;
-//    }
+    return  bb->x - aa->x;
 }
 
-int merge_sort(Point *arr,int start,int mid,int last)
+long long int merge_sort(Point *arr,int start,int mid,int last)
 {
-//    int *temp = new int[mid - start];
-//    temp -= start;
+
     for(int i = start;i<mid;++i)
     {
         temp[i] = arr[i].y;
     }
     int k = start;
     long long int reverse_order = 0;
-//    printf("start:%d mid:%d last:%d\n",start,mid,last);
+
     for(int i = start, j = mid ; i < mid ; )
     {
 
@@ -41,7 +32,6 @@ int merge_sort(Point *arr,int start,int mid,int last)
         {
             arr[k++].y = arr[j++].y;
             reverse_order += (mid-i);
-//            ++j;
         }
 
         if(last <= j || temp[i]<arr[j].y)
@@ -49,12 +39,10 @@ int merge_sort(Point *arr,int start,int mid,int last)
             arr[k++].y= temp[i++];
         }
     }
-//    printf("%d\n",reverse_order);
-//    delete []temp;
     return reverse_order;
 }
 
-int _merge(Point * arr, int start, int last)
+long long int _merge(Point * arr, int start, int last)
 {
     if(start +1 == last)
     {
@@ -62,7 +50,6 @@ int _merge(Point * arr, int start, int last)
     }
     long long int reverse_order=0;
     int mid = (start + last)>>1;
-//    printf("%d\n",mid);
     reverse_order += _merge(arr,start,mid);
     reverse_order += _merge(arr,mid,last);
     return reverse_order +  merge_sort(arr,start,mid,last);
@@ -75,17 +62,15 @@ int main()
 
     scanf("%d",&lamb_tower);
 
-//    while(lamb_tower --)
     for(int i =0;i<lamb_tower;++i)
     {
         scanf("%d%d",&(P[i].x),&(P[i].y));
     }
     qsort(P,lamb_tower, sizeof(P[0]),cmp);
     long long int reverse_order = _merge(P,0,lamb_tower);
+
     printf("%lld\n",reverse_order);
-//    for(int i =0;i<lamb_tower;++i)
-//    {
-//
-//        printf("%d %d\n",P[i].x,P[i].y);
-//    }
+
+    return 0;
+
 }
